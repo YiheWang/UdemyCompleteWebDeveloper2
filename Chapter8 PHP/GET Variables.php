8734b5aed796c93b1e63f63ee30fca13d444dea8
YiheWang <ywm46@mail.missouri.edu>
2019-08-04 19:39:21 +0800

    Create GET Variables.php

---
+++ Chapter8 PHP/GET Variables.php
@@ -0,0 +1,33 @@
+<?php
+    /*print_r($_GET);
+    echo "Hi there ".$_GET["name"]."!";*/
+
+    if(is_numeric($_GET["number"]) && $_GET["number"] > 0 && $_GET["number"] == round($_GET["number"], 0)) {
+        $i = 2;
+        $isPrime = true;
+        while ($i < $_GET["number"]) {
+            if($_GET["number"] % $i == 0){
+                //number is not prime
+                $isPrime = false;
+            }
+            $i++;
+        }
+        if($isPrime) {
+            echo "<p>".$i." is a prime number!</p>";
+        }
+        else{
+            echo "<p>".$i." is not a prime number</p>";
+        }
+    }
+    else {
+        echo "<p>Please enter a positive whole number</p>";
+    }
+
+
+?>
+
+<p>Please enter a whole number</p>
+<form>
+    <input name ="number" type="text">
+    <input type="submit" value="Go!">
+</form>
