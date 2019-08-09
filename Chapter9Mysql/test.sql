8f11bef3888c0f882e0e98a70b49d22f52bb9608
YiheWang <ywm46@mail.missouri.edu>
2019-08-09 21:54:51 +0800

    sign up , loop through data

--- Chapter9Mysql/test.sql
+++ Chapter9Mysql/test.sql
@@ -5,3 +5,5 @@ INSERT INTO users ('id', 'email', 'password') VALUES (4,'haha@qq.com','hshjshd')
 INSERT INTO users (id, email, password) VALUES (4,'haha@qq.com','hshjshd')
 
 INSERT INTO users (email, password) VALUES ('haha@qq.com','hshjshd')
+
+SELECT id FROM users WHERE email = 'hahayw@qq.com';
