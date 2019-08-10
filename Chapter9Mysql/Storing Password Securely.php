d9c5f9898f89d3c549eee601a7695fc1245a72fa
YiheWang <ywm46@mail.missouri.edu>
2019-08-10 16:59:24 +0800

    Create Storing Password Securely.php

---
+++ Chapter9Mysql/Storing Password Securely.php
@@ -0,0 +1,13 @@
+<?php
+    //level 2 store
+    $salt = "duknfkljabu3h2o3i8fwi98fnj";
+    echo md5($salt."password")."<br>";
+    // some commonly used password can be easily decode, put salt in front of the string
+    //make it more secure
+
+    
+    //level 3 store
+    $row["id"] = 73;
+    echo md5(md5($row["id"])."password");
+    //row id is static per user, so the salt string is always changing
+?>
