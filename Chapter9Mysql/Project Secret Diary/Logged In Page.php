76d9589814e63f79a6b1e875493fd0257f5253a5
YiheWang <ywm46@mail.missouri.edu>
2019-08-10 23:23:31 +0800

    set session and cookie finished

---
+++ Chapter9Mysql/Project Secret Diary/Logged In Page.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    if (array_key_exists("id",$_COOKIE)) {
+        $_SESSION["id"] = $_COOKIE["id"];
+        //pass cookie id to session id
+        //session id was usually stored in cookie
+    }
+    if (array_key_exists("id",$_SESSION)) {
+        echo "<p>Logged In! <a href='Project%20Secret%20Diary.php?logout=1'>Log out</a></p>";
+        //?logout=1 is used to pass GET variables, logout
+    }//logged in case
+    else {
+        header("Location: Project%20Secret%20Diary.php");
+    }// not logged in case. For example, people want to access to diary url
+    //without logging in. In that case, page will be redirect to home page
+?>
--- Chapter9Mysql/Project Secret Diary/Project Secret Diary.php
+++ Chapter9Mysql/Project Secret Diary/Project Secret Diary.php
@@ -4,7 +4,22 @@
     $userName = "epiz_22438332";
     $password = "yL8UIUdFE";
     $databaseName = "epiz_22438332_Secret_Diary";
-    //check if submit in array $_POST
+    $error = "";
+
+    session_start();
+    if (array_key_exists("logout",$_GET)) {
+        unset($_SESSION);
+        setcookie("id", "", time() - 60 * 60 * 24);
+        //remove a cookie
+        $_COOKIE["id"] = "";
+        //set to empty in case it gets accessed later on
+    }//check if logged put
+    else if (array_key_exists("id",$_SESSION)
+        OR array_key_exists("id",$_COOKIE)){
+        header("Location: Logged%20In%20Page.php");
+    }// id already logged in. Taking id to logged in page
+
+    //check if name submit in array $_POST, which means did user click submit
     if (array_key_exists("submit",$_POST)) {
         //print_r($_POST);
         $link = mysqli_connect($hostName, $userName, $password, $databaseName);
@@ -12,7 +27,6 @@
             die ("Database Connection Error!");
         }// check if connecting successful
 
-        $error = "";
         if (!$_POST["email"]) {
             $error = "An email address is required!<br>";
         }
@@ -21,7 +35,7 @@
         }
         if($error != "") {
             $error = "<p>There were errors(s) in your form:</p>".$error;
-        }
+        }//check if user put in their email and password
         else {
             $query = "SELECT id FROM users WHERE email = '".
                 mysqli_real_escape_string($link, $_POST["email"])."' LIMIT 1";
@@ -44,10 +58,18 @@
                     $query = "UPDATE users SET password = '".$passwordHash."' WHERE id = ".
                     mysqli_insert_id($link)." LIMIT 1";
                     //mysqli_insert_id($link) returns the id of most recently used element
-                    
+
                     //update the password to make it more securely
                     mysqli_query($link, $query);
-                    echo "<p>Sign up successful!</p>";
+
+                    $_SESSION["id"] = mysqli_insert_id($link);
+                    if ($_POST["stayLoggedIn"] == '1') {
+                        setcookie("id",mysqli_insert_id($link),time() + 60 * 60 * 24);
+                        //set cookie for a day
+                    }//checkbox is selected when equal to 1
+
+                    header("Location: Logged%20In%20Page.php");
+                    //sign up and take you to logged in page
                 }//query run successful; sign up successful
             }
         }
@@ -57,7 +79,7 @@
 <form method="post">
     <input type="email" name="email" placeholder="Your email">
     <input type="password" name="password" placeholder="Password">
-    <input type="checkbox" name="stayLoggedIn" value=1>
+    <input type="checkbox" name="stayLoggedIn" value='1'>
     <input type="submit" name="submit" value="Sign Up!">
 </form>
 <div id="error"><?php echo $error; ?></div>
