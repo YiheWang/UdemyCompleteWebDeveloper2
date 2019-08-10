1dd465807a61c7b7d9425dddc5b89777f1e19d14
YiheWang <ywm46@mail.missouri.edu>
2019-08-10 17:07:46 +0800

    more secure way use hash method

--- Chapter9Mysql/Storing Password Securely.php
+++ Chapter9Mysql/Storing Password Securely.php
@@ -5,9 +5,19 @@
     // some commonly used password can be easily decode, put salt in front of the string
     //make it more secure
 
-    
+
     //level 3 store
     $row["id"] = 73;
     echo md5(md5($row["id"])."password");
     //row id is static per user, so the salt string is always changing
+
+    // here is more secure method in PHP 5.5
+    $hash = password_hash("myPassword",PASSWORD_DEFAULT);
+    echo $hash."<br>";
+    if (password_verify("myPassword",$hash)) {
+        echo "password is valid!";
+    }
+    else {
+        echo "Invalid password!";
+    }
 ?>
